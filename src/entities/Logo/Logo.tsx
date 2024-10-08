79815a0a51c80e87c56e1ed10f6d701aec53ff16
dan83g <71218760+dan83g@users.noreply.github.com>
2024-10-08 09:30:53 +0000

    Automatically publish Storybook

--- src/entities/Logo/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import shoppingCartLogo from '../../assets/shopping-cart.svg';
-import s from './Logo.module.sass';
-
-const Logo = () => {
-  return <img src={shoppingCartLogo} className={s['logo']} alt="logo" />;
-};
-
-export default Logo;
